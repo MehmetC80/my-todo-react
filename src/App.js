5f06ca9d9cde616a8c7a28ee1c6fbae0c75f1f5c
Mehmet Cetinkaya <m.cetinkaya@hotmail.de>
2022-04-16 14:34:55 +0200

    add PATCH request

--- src/App.js
+++ src/App.js
@@ -42,11 +42,25 @@ function App() {
     }, 2000);
   }, []);
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'appilcation/json',
+      },
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    };
+
+    const reqUrl = `${ApiUrl}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
   };
 
   const handleDelete = (id) => {
